1d0cab66681334980765f0e267f07643552fe3f6
zvasileva <zojka-homka@rambler.ru>
2020-06-10 23:57:09 +0300

    Готово

--- src/js/__tests__/task.test.js
+++ src/js/__tests__/task.test.js
@@ -2,22 +2,22 @@ import Character from '../task';
 
 test.each([
   ['Ivan', 'Bowman', {
-    name: 'Ivan', type: 'Bowman', health: 100, level: 1, attack: '25/25',
+    name: 'Ivan', type: 'Bowman', health: 100, level: 1, attack: 25, defence: 25,
   }],
   ['Igor', 'Swordsman', {
-    name: 'Igor', type: 'Swordsman', health: 100, level: 1, attack: '40/10',
+    name: 'Igor', type: 'Swordsman', health: 100, level: 1, attack: 40, defence: 10,
   }],
   ['Petr', 'Magician', {
-    name: 'Petr', type: 'Magician', health: 100, level: 1, attack: '10/40',
+    name: 'Petr', type: 'Magician', health: 100, level: 1, attack: 10, defence: 40,
   }],
   ['Zara', 'Undead', {
-    name: 'Zara', type: 'Undead', health: 100, level: 1, attack: '25/25',
+    name: 'Zara', type: 'Undead', health: 100, level: 1, attack: 25, defence: 25,
   }],
   ['Lukas', 'Zombie', {
-    name: 'Lukas', type: 'Zombie', health: 100, level: 1, attack: '40/10',
+    name: 'Lukas', type: 'Zombie', health: 100, level: 1, attack: 40, defence: 10,
   }],
   ['Denys', 'Daemon', {
-    name: 'Denys', type: 'Daemon', health: 100, level: 1, attack: '10/40',
+    name: 'Denys', type: 'Daemon', health: 100, level: 1, attack: 10, defence: 40,
   }],
 ])('get result', (name, type, expected) => {
   const result = new Character(name, type);
--- src/js/task.js
+++ src/js/task.js
@@ -15,16 +15,22 @@ export default function Character(name, type) {
   this.level = 1;
 
   if (type === 'Bowman') {
-    this.attack = '25/25';
+    this.attack = 25;
+    this.defence = 25;
   } else if (type === 'Swordsman') {
-    this.attack = '40/10';
+    this.attack = 40;
+    this.defence = 10;
   } else if (type === 'Magician') {
-    this.attack = '10/40';
+    this.attack = 10;
+    this.defence = 40;
   } else if (type === 'Undead') {
-    this.attack = '25/25';
+    this.attack = 25;
+    this.defence = 25;
   } else if (type === 'Zombie') {
-    this.attack = '40/10';
+    this.attack = 40;
+    this.defence = 10;
   } else if (type === 'Daemon') {
-    this.attack = '10/40';
+    this.attack = 10;
+    this.defence = 40;
   }
 }
